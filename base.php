4da4e016edd6569caa83beb78b6554bb3211c496
eapl.mx <eapl.mx@gmail.com>
2023-07-06 18:46:22 -0600

    Updated parameters for cookies.

--- base.php
+++ base.php
@@ -3,6 +3,18 @@ declare(strict_types=1);
 session_name('twtxt');
 session_start([
 	'name' => 'twtxt',
+	'use_strict_mode' => true,
+	'cookie_httponly' => true,
 	'cookie_secure' => true,
 	'cookie_lifetime' => 604800, // 7 days
+	'gc_maxlifetime' => 604800, // 7 days
+	'sid_length' => 64,
+	'sid_bits_per_character' => 6,
 ]);
+
+// TODO: Implement improvements to session
+// https://www.php.net/manual/en/function.session-regenerate-id.php
+// https://stackoverflow.com/questions/1236374/session-timeouts-in-php-best-practices
+
+// Add csrf protection like on
+// https://hg.sr.ht/~m15o/mebo/browse/classes/Session.php?rev=tip

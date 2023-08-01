4b6a45ea374ae16f32855f686c7efa8339759608
eapl.mx <eapl.mx@gmail.com>
2023-08-01 13:50:39 -0600

    Arranged test folder.

--- tests/links_pattern.php
+++ tests/links_pattern.php
@@ -1,5 +1,11 @@
 <?php
-$string = 'This is an URL: https://ea.com gemini://example.com https://youtu.be/ysaNUatLMn0 https://eapl.me/twtxt.txt Friends... OpenBSD.. https://ea.com<br>';
+$string = 'This is an URL: https://ea.com gemini://example.com https://youtu.be/ysaNUatLMn0 https://eapl.me/twtxt.txt Friends... OpenBSD.. https://ea.com<br> eapl.me 8.2';
+
+/*
+// Broken links:
+<a href="eapl.me">eapl.me</a>
+<a href="8.2">8.2</a>
+*/
 
 // Regular expression pattern to match URLs
 //$pattern = '/(?<!\S)(\b(https?|ftp|gemini|spartan|gopher):\/\/\S+|\S+\.\S+\.\S+)(?!\S)/';

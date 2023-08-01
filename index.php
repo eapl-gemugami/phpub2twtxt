e62931f453318f25c72c06269d10035e4b56a737
eapl.mx <eapl.mx@gmail.com>
2023-08-01 14:01:23 -0600

    Fixed favicon.

--- index.php
+++ index.php
@@ -120,6 +120,14 @@ $twts = array_slice($twts, $startingTwt, TWTS_PER_PAGE);
 	<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="manifest" href="site.webmanifest">
+	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+	<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="apple-mobile-web-app-title" content="twtxt">
+	<meta name="application-name" content="twtxt">
+	<meta name="msapplication-TileColor" content="#2b5797">
+	<meta name="theme-color" content="#2b5797">
 </head>
 <body>
 	<h1><a href=".">twtxt</a></h1>

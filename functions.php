4db0166bf36808e3e7e7ac6fe6be31b81422ebc4
eapl.mx <eapl.mx@gmail.com>
2023-08-08 12:00:57 -0600

    Fixed error on PHP 8.2 for ini_set

--- functions.php
+++ functions.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 $config = parse_ini_file('.config');
 
 if ($config['debug_mode']) {
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
+	ini_set('display_errors', '1');
+	ini_set('display_startup_errors', '1');
 	error_reporting(E_ALL);
 }
 

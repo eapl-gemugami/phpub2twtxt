ff104a5ce22e26ae1a14a83ac14c1d0d7384694e
eapl.mx <eapl.mx@gmail.com>
2023-08-22 16:31:30 -0600

    Improve new_txt.php

--- new_twt.php
+++ new_twt.php
@@ -34,36 +34,46 @@ if (isset($_POST['submit'])) {
 	// Remove Carriage return if needed
 	$new_post = str_replace("\r", '', $new_post);
 
+	// TODO: If twt is emply, show an error
+	/*
 	if ($new_post) {
-		// Check if we have a point to insert the next Twt
-		define('NEW_TWT_MARKER', "#~~~#\n");
-		$contents = file_get_contents($txt_file_path);
+	}
+	*/
 
-		if (!date_default_timezone_set($timezone)) {
-			date_default_timezone_set('UTC');
-		}
+	// Check if we have a point to insert the next Twt
+	define('NEW_TWT_MARKER', "#~~~#\n");
 
-		$twt = date('c') . "\t$new_post\n";
-
-		if (strpos($contents, NEW_TWT_MARKER) !== false) {
-			// Add the previous marker
-			$twt = NEW_TWT_MARKER . $twt;
-			$contents = str_replace(NEW_TWT_MARKER, $twt, $contents);
-		} else {
-			$contents .= $twt;
-		}
-
-		// TODO: Add error handling if write to the file fails
-		// For example due to permissions problems
-		$file_write_result = file_put_contents($txt_file_path, $contents);
-
-		//header('Refresh:0; url=?');
-		header('Refresh:0; url=.');
-		exit;
-	} else {
-		echo "Oops something went wrong...\n\nCheck the error_log on the server";
+	if (!file_exists($txt_file_path)) {
+		echo 'twtxt.txt file does not exist. Check your config.';
 		exit;
 	}
+
+	$contents = file_get_contents($txt_file_path);
+
+	if (!date_default_timezone_set($timezone)) {
+		date_default_timezone_set('UTC');
+	}
+
+	$twt = date('c') . "\t$new_post\n";
+
+	if (strpos($contents, NEW_TWT_MARKER) !== false) {
+		// Add the previous marker
+		// Take note that doesn't not work if twtxt file has CRLF line ending
+		// (which is wrong anyway)
+		$twt = NEW_TWT_MARKER . $twt;
+		$contents = str_replace(NEW_TWT_MARKER, $twt, $contents);
+	} else {
+		// Fall back if the marker is not found.
+		$contents .= $twt;
+	}
+
+	// TODO: Add error handling if write to the file fails
+	// For example due to permissions problems
+	// https://www.w3docs.com/snippets/php/how-can-i-handle-the-warning-of-file-get-contents-function-in-php.html
+	$file_write_result = file_put_contents($txt_file_path, $contents);
+
+	header('Refresh:0; url=.');
+	exit;
 } else { ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -78,7 +88,7 @@ if (isset($_POST['submit'])) {
 	<form method="POST" class="column">
 		<div id="posting">
 			<textarea class="textinput" id="new_post" name="new_post"
-				rows="4" cols="100" autofocus
+				rows="4" cols="100" autofocus required
 				placeholder="Your twt"><?= $textareaValue ?></textarea>
 			<br>
 			<input class="btn" type="submit" value="Post" name="submit">
